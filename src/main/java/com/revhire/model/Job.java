7189bc8837bb09171cda69db22da0f68f2ccdc48
Bhanu5355 <bhukyabhanu1234@gmail.com>
2026-02-22 10:42:32 +0530

    Updated job and users entity

--- src/main/java/com/revhire/model/Job.java
+++ src/main/java/com/revhire/model/Job.java
@@ -1,5 +1,55 @@
 package com.revhire.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Job {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String companyName;
+    private String location;
+    private String salary;
+    private String description;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+    
 
 }
